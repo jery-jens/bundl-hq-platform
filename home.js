1abcbe86bb98f0e3d7fbdf30c9c8f3e842724af6
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-04-15 16:06:07 +0200

    Updated fireworks

--- home.js
+++ home.js
@@ -1,4 +1,22 @@
-const container = document.querySelector('.fireworks');
-const fireworks = new Fireworks(container, { speed: 1 });
+const container = document.querySelector('body');
+const fireworks = new Fireworks(
+  container,
+  {
+    acceleration: 1,
+    brightness:
+    {
+      min: 50,
+      max: 73,
+      decay: {
+        min: 0.0015,
+        max: 0.013,
+      },
+    },
+    explosion: 10,
+    intensity: 4.85,
+    gravity: 0.65,
+    opacity: 0.5,
+  },
+);
 
 fireworks.start();
